3a7956f77d95922bf7602c064b43d5b3c7b4a3b1
Cleidison Silva <cleidison.dev@gmail.com>
2023-09-24 01:32:45 -0300

    feat: Add Home route

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,28 @@ import {
 	createRoutesFromElements,
 } from 'react-router-dom';
 import { Login } from './pages/Login';
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
+
+const user = true;
 
 export const routes = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/'>
-			<Route index element={<Login />} />
+			<Route index element={user ? <Navigate to='home' /> : <Login />} />
+
+			{user && (
+				<Route>
+					<Route
+						path='home'
+						element={
+							<Layout title='Início'>
+								<Home />
+							</Layout>
+						}
+					/>
+				</Route>
+			)}
 
 			<Route path='*' element={<Navigate to='/' />} />
 		</Route>
