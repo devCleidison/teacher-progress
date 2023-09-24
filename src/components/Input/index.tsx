1a5efd936c285294830b461fd5b475a2efc7e5db
Cleidison Silva <cleidison.dev@gmail.com>
2023-09-23 23:44:30 -0300

    feat: Add handlePasswordInputType function for show and hide user password

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,5 +7,24 @@ export const Input = {
 	Root: InputRoot,
 	Icon: InputIcon,
 	Text: InputText,
-	Label: InputLabel
+	Label: InputLabel,
 };
+
+
+type IProps = {
+	id: string;
+	state: boolean;
+	setState: (value: boolean) => void;
+};
+
+export function handlePasswordInputType({ id, setState, state }: IProps) {
+	const inputPass = document.querySelector(id);
+
+	if(state) {
+		setState(false);
+		inputPass?.setAttribute('type', 'password');
+	} else {
+		setState(true);
+		inputPass?.setAttribute('type', 'text');
+	}
+}
