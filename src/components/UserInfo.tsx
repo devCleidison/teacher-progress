4774dd323012a2dca13f48e76503830dd8496c5f
Cleidison Silva <cleidison.dev@gmail.com>
2023-09-26 22:51:49 -0300

    feat: Use dynamic user data

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,24 +1,19 @@
 import { School, User } from 'lucide-react';
-
-const user: IUser = {
-	id: '1',
-	name: 'Cleidison Silva',
-	email: 'cleidison.dev@gmail.com',
-	avatarUrl: 'https://avatars.githubusercontent.com/u/67656036?v=4',
-	type: 'admin',
-};
+import { useAuth } from '../hooks/useAuth';
 
 export function UserInfo() {
+	const { user } = useAuth();
+
 	return (
 		<div className='flex items-center gap-3'>
 			<div className='flex items-center justify-center rounded-full overflow-hidden w-11 h-11 bg-zinc-100 text-zinc-400'>
-				{user.type === 'admin' ? (
+				{user?.type === 'admin' ? (
 					user.avatarUrl ? (
 						<img src={user.avatarUrl} alt={`Foto de ${user.name}`} />
 					) : (
 						<User />
 					)
-				) : user.avatarUrl ? (
+				) : user?.avatarUrl ? (
 					<img src={user.avatarUrl} alt={`Foto de ${user.name}`} />
 				) : (
 					<School />
@@ -26,8 +21,8 @@ export function UserInfo() {
 			</div>
 
 			<div className='flex flex-col'>
-				<span className='font-medium leading-5'>{user.name}</span>
-				<span className='text-sm text-zinc-500 leading-5'>{user.email}</span>
+				<span className='font-medium leading-5'>{user?.name}</span>
+				<span className='text-sm text-zinc-500 leading-5'>{user?.email}</span>
 			</div>
 		</div>
 	);
