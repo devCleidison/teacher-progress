e7719665a11ce3a8c7e6071a4dd72c473954ac7c
Cleidison Silva <cleidison.dev@gmail.com>
2023-09-26 22:03:47 -0300

    feat: Create AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,66 @@
+import { ReactNode, createContext, useEffect, useState } from 'react';
+import {
+	browserSessionPersistence,
+	onAuthStateChanged,
+	setPersistence,
+	signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '../services/firebase';
+import { useError } from '../hooks/useError';
+
+type IAuthContext = {
+	isAuthenticated: boolean;
+	user: IUser | null;
+	isLoading: boolean;
+	handleLogin: (email: string, password: string) => Promise<void>;
+};
+
+type IAuthProvider = {
+	children: ReactNode;
+};
+
+export const AuthContext = createContext({} as IAuthContext);
+
+export function AuthProvider({ children }: IAuthProvider) {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [user, setUser] = useState<IUser | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const { setError } = useError();
+
+	async function handleLogin(email: string, password: string) {
+		setIsLoading(true);
+		setPersistence(auth, browserSessionPersistence)
+			.then(() => {
+				return signInWithEmailAndPassword(auth, email, password);
+			})
+			.catch(() => {
+				setError('Usuário ou senha inválidos!');
+			})
+			.finally(async () => {
+				if(auth.currentUser) {
+					setIsAuthenticated(true);
+				}
+				setIsLoading(false);
+			});
+	}
+
+	useEffect(() => {
+		onAuthStateChanged(auth, async (isLogged) => {
+			if(isLogged) {
+				setIsAuthenticated(true);
+			} else {
+				setIsAuthenticated(false);
+				setUser(null);
+			}
+		});
+	},[auth]);
+
+	return (
+		<AuthContext.Provider
+			value={{ isAuthenticated, user, isLoading, handleLogin }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+}
